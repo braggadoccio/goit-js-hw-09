806330a80ca80c87c2822e03bf56495931b8ba37
Paulo <pau.solanoy@gmail.com>
2023-11-17 21:21:51 +0800

    Commit Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+
+let timerId = null;
+startButton.disabled = true;
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopButtonClick() {
+  clearInterval(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
