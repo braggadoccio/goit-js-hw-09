c0d2921a798c233045486476e9f6d656151b2439
braggadoccio <137191651+braggadoccio@users.noreply.github.com>
2023-11-18 15:12:47 +0000

    Deploying to gh-pages from @ braggadoccio/goit-js-hw-09@b67f79b68f3d8d4f12c63c6baa6533562e78697f 🚀

---
+++ 01-color-switcher.62c6b535.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;t.disabled=!1,e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.62c6b535.js.map
